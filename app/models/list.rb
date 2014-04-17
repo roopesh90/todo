7b68eea5ce12ce76dc95c5d8abf1f468740cd605
roopesh90 <roopesh90nair@live.com>
2014-04-17 15:10:34 +0530

    validation included to pass test 1,2

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,8 @@ class List
 	include Mongoid::Document
 	field :priority, type: Integer
 	field :content, type: String
+	
+	validates :content, presence: true
+	validates :priority, presence: true
+
 end
