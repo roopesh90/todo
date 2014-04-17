520c75ac4e326e87e4abd1a2723ce3d53ec601b4
roopesh90 <roopesh90nair@live.com>
2014-04-17 15:23:14 +0530

    secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+ 
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_key_base = "fbjuknnbnj3gmcrhtmmtpc56agznafwh2df9fs5pgwdfesxbujdmsq4ck9zwvq45mwnue6jdbrugvxacu9s52adkxt9mx2jf8t4s"
