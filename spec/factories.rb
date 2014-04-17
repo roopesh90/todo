6581dabb62665cd092cdcef6944d2993c2bdc4d4
roopesh90 <roopesh90nair@live.com>
2014-04-17 16:17:37 +0530

    Here comes facker, Thank you http://github.com/stympy

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,8 @@
+require 'faker'
+
 FactoryGirl.define do
-   factory :list do
-      content 'TestList'
-      priority { 1 + rand(10) }
+   factory :list do |l|
+      l.content { Faker::Lorem.sentence }
+      l.priority { Faker::Number.digit }
    end
 end
