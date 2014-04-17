3209fcec756f76bfc44783f8b0b531863bcdfaec
roopesh90 <roopesh90nair@live.com>
2014-04-17 15:08:41 +0530

    testcases to fail

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe List do
-
+	it "is invalid without priority" do
+		FactoryGirl.build(:list, :priority => nil).should_not be_valid
+	end
+	
+	it "is invalid without content" do
+		list = FactoryGirl.build(:list, :content => nil)
+		list.should_not be_valid  
+	end
 end
 
